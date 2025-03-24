d989b90f5de0790fdbfc8d0b55540e74609cad9c
Nathaniel Jackson <njackson@student.42adel.org.au>
2025-03-24 16:17:34 +1030

    fix gen-ssl.sh

--- containers/nginx/scripts/gen-ssl.sh
+++ containers/nginx/scripts/gen-ssl.sh
@@ -1,5 +1,5 @@
 
-if [ ! -f /etc/ssl/inception_private_key.key ]; then
+if [ ! -f /etc/ssl/website_ssl.key ]; then
     openssl req -newkey rsa:2048 -noenc\
         -keyout /etc/ssl/website_ssl.key -x509 -days 365\
         -out /etc/ssl/certs/website_ssl.csr\
