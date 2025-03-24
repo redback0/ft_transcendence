6d02fecb0f3fae922b9a4e2816a172a64b9c02eb
Nathaniel Jackson <njackson@student.42adel.org.au>
2025-03-24 16:39:13 +1030

    Update gen-ssl script for debugging

--- containers/nginx/scripts/gen-ssl.sh
+++ containers/nginx/scripts/gen-ssl.sh
@@ -3,5 +3,6 @@ if [ ! -f /etc/ssl/website_ssl.key ]; then
     openssl req -newkey rsa:2048 -noenc\
         -keyout /etc/ssl/website_ssl.key -x509 -days 365\
         -out /etc/ssl/certs/website_ssl.csr\
-        -subj '/CN='${SERVER_NAME}'' > /dev/null
+        -subj '/CN='${SERVER_NAME}'' > /dev/null 2>&1 && \
+        echo "key generated" || echo "key error"
 fi
