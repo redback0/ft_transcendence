641df7fde081f1c6bf9a81ab5c37078552e7f7a3
Reuben Kabzinski <rkabzins@student.42adel.org.au>
2025-04-01 19:10:31 +1030

    Made it work for my Windows machine, hopefully it also works on unix based machines still

--- containers/nginx/scripts/gen-ssl.sh
+++ containers/nginx/scripts/gen-ssl.sh
@@ -1,9 +1,10 @@
+#!/bin/sh
 
 if [ ! -f /etc/ssl/website_ssl.key ]; then
-    openssl req -newkey rsa:2048 -noenc\
-        -keyout /etc/ssl/website_ssl.key -x509 -days 365\
-        -out /etc/ssl/certs/website_ssl.csr\
-        -subj '/CN='${SERVER_NAME}''
+    openssl req -newkey rsa:2048 -noenc \
+        -keyout /etc/ssl/website_ssl.key -x509 -days 365 \
+        -out /etc/ssl/certs/website_ssl.csr \
+        -subj "/CN=${SERVER_NAME}"
 else
     echo "Key already generated"
 fi
