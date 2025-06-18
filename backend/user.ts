44baedd92bcd626dd94af16c72218d6da02a85d5
Nicole Lehmeyer <nicole.lehmeyer@icloud.com>
2025-06-18 15:56:19 +0930

    Added user.ts

---
+++ backend/user.ts
@@ -0,0 +1,19 @@
+// Authored by Nicole L, 18 June 2025
+
+class User {
+	username:		string;
+	pw:				string;
+	pwHash:			string;
+	pwSalt:			string
+	onlineStatus:	boolean;
+	uid:			number;
+	longestRally:	boolean;
+	numLosses:		number;
+	numWins:		number;
+	dateCreated:	string;
+	dateLastLogin:	string;
+
+	//gameHistory;
+	//friends
+	//avatar
+}
