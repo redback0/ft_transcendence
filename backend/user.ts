5f315030bc9913a83ca8af198f2949106a1d1891
Nicole Lehmeyer <nicole.lehmeyer@icloud.com>
2025-06-18 16:41:22 +0930

    Added personal notes

--- backend/user.ts
+++ backend/user.ts
@@ -2,7 +2,6 @@
 
 class User {
 	username:		string;
-	pw:				string;
 	pwHash:			string;
 	pwSalt:			string
 	onlineStatus:	boolean;
@@ -12,8 +11,29 @@ class User {
 	numWins:		number;
 	dateCreated:	string;
 	dateLastLogin:	string;
+	avatar:			string;
 
 	//gameHistory;
 	//friends
-	//avatar
 }
+
+// 1. Session tokens - the idea, not the term
+// 2. SQL Crash course
+// 3. People to create users in the database - Work with Jack (SQL)
+// 4. Create and modify users
+
+// ** CONCEPTS TO STUDY **
+//		Web requests
+//		Hashing and salting
+//		SQL (& TS)
+//		Caching
+
+// Cache number of wins and losses
+// Every request on the backend needs to be authenticated
+// It could come from not the frontend. You need to be always checking
+// Anything that can be done on the frontend to the backend must have security behind it
+// The backend must be self authenticating
+// If someone is trying to modify their details, that they are changing their own details
+// Session token (?) 
+
+//
