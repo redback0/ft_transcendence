175a0e6470783ad88470e4f40f64dba4745de785
redback0 <nate@thejackos.net>
2025-06-21 09:50:07 +0930

    Database is now a seperate volume, add container to fix permissions

--- backend/server.ts
+++ backend/server.ts
@@ -4,7 +4,7 @@ import { initChat, chatWebSocketServer } from './chat.js';
 import * as Game from './game.js';
 import Database from 'better-sqlite3';
 
-const db = new Database('/backend/database/pong.db');
+const db = new Database('/database/pong.db');
 export const fastify: FastifyInstance = Fastify({});
 // all the requests to the backend should go through /api
 fastify.get('/api/buttonpressed', function handler(request, reply)
