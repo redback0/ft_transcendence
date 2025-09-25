d7c877cecbfc9a9a244efad2b5ea09541f37d8b0
Jack Church <47371784+jackchurch@users.noreply.github.com>
2025-09-25 13:51:08 +0930

    more friends setup

--- backend/friend.logic.ts
+++ backend/friend.logic.ts
@@ -3,6 +3,47 @@ import { db } from "./database";
 import { Friend } from "./friend.schema";
 import { SESSION_ID_COOKIE_NAME, sidToUserIdAndName, getUserInfo } from "./cookie";
 
+
+// Setup
+export async function checkFriendRecordExists(myId: string, theirId: string): Promise<boolean>
+{
+		const statement = db.prepare(`SELECT 1 FROM friend WHERE my_id = ? AND friend_id = ? LIMIT 1;`);
+		const result = statement.get(myId, theirId);
+		if (result)
+		{
+			console.log(`${myId} has a friend record on ${theirId}`);
+			return (true);
+		}
+		console.log(`${myId} has no friend record on ${theirId}`);
+		return (false);
+}
+
+export async function setupFriendRecordIfNotExist(myId: string, theirId: string): Promise<boolean>
+{
+	if (await checkFriendRecordExists(myId, theirId) === true)
+	{
+		console.log(`Friend record alread exists`);
+		return (true);
+	}
+	try
+	{
+		db.prepare('BEGIN TRANSACTION').run();
+		const statement = db.prepare(`INSERT INTO friend (my_id, friend_id)
+			VALUES (?, ?)`);
+		statement.run(myId, theirId);
+		db.prepare('COMMIT').run();
+		console.log(`Friend records setup.`);
+		return (true);
+	}
+	catch (error)
+	{
+		db.prepare('ROLLBACK').run();
+		console.error('Error: Cannot setup friend record.', error);
+		return (false);
+	}
+}
+
+// Block
 export async function getFriendsFromDatabase(myId: string): Promise<Friend[] | null>
 {
 	try
@@ -28,7 +69,7 @@ export async function getFriendsIBlockedFromDatabase(myId: string): Promise<Frie
 	}
     catch (error)
     {
-		console.log(`Cannot select frinds from database: ${error}`);
+		console.log(`Error getFriendsIBlockedFromDatabase: ${error}`);
 		return (null);
 	}
 }
@@ -47,7 +88,7 @@ export async function getDidIBlockThemFromDatabase(myId: string, theirId: string
 	}
     catch (error)
     {
-		console.log(`Error getDidTheyBlockMeFromDatabase: ${error}`);
+		console.log(`Error getDidIBlockThemFromDatabase: ${error}`);
 		return (false);
 	}
 }
@@ -59,14 +100,126 @@ export async function getDidTheyBlockMeFromDatabase(myId: string, theirId: strin
 		const statement = db.prepare(`SELECT 1 FROM friend WHERE my_id = ? AND friend_id = ? AND blocked_by_me = 1 LIMIT 1;`);
 		const result = statement.get(theirId, myId);
 		if (result)
-		{
 			return (true);
-		}
 		return (false);
 	}
     catch (error)
     {
 		console.log(`Error getDidTheyBlockMeFromDatabase: ${error}`);
+		return (false);
+	}
+}
+
+
+export async function setIBlockThem(myId: string, theirId: string): Promise<boolean>
+{
+	await setupFriendRecordIfNotExist(myId, theirId);
+	try
+	{
+		db.prepare(`BEGIN TRANSACTION`).run();
+		const statement = db.prepare(`UPDATE friend SET blocked_by_me = 1, friend_status = NULL WHERE my_id = ? AND friend_id = ?;`);
+		statement.run(myId, theirId);
+		db.prepare(`COMMIT`).run();
+		console.log(`Friend blocked: ${theirId}`);
+		return (true);
+	}
+	catch (error)
+	{
+		db.prepare('ROLLBACK').run();
+		console.error(`Cannot block person ${theirId}`);
+		return (false);
+	}
+}
+
+export async function setIUnblockThem(myId: string, theirId: string): Promise<boolean>
+{
+	await setupFriendRecordIfNotExist(myId, theirId);
+	try
+	{
+		db.prepare(`BEGIN TRANSACTION`).run();
+		const statement = db.prepare(`UPDATE friend SET blocked_by_me = 0 WHERE my_id = ? AND friend_id = ?;`);
+		statement.run(myId, theirId);
+		db.prepare(`COMMIT`).run();
+		console.log(`Friend unblocked: ${theirId}`);
+		return (true);
+	}
+	catch (error)
+	{
+		db.prepare('ROLLBACK').run();
+		console.error(`Cannot unblock person ${theirId}`);
+		return (false);
+	}
+}
+
+
+// Let's make friends slowly 
+export async function checkAreTheyWaitingForFriendAproval(myId: string, theirId: string): Promise<boolean>
+{
+	try
+	{
+		const statement = db.prepare(`SELECT 1 FROM friend WHERE my_id = ? AND friend_id = ? AND friend_status = 0 LIMIT 1;`);
+		const result = statement.get(theirId, myId);
+		if (result)
+			return (true);
+		return (false);
+	}
+	catch (error)
+	{
+		return (false);
+	}
+}
+
+
+
+export async function requestFriendship(myId: string, theirId: string): Promise<boolean>
+{
+	await setupFriendRecordIfNotExist(myId, theirId);
+
+	if (await getDidTheyBlockMeFromDatabase(myId, theirId) === true)
+	{
+		console.log(`There will be cake.`);
+		return (false);
+	}
+	try
+	{
+		db.prepare(`BEGIN TRANSACTION`).run();
+		const statement = db.prepare(`UPDATE friend set friend_status = 0, blocked_by_me = 0 WHERE my_id = ? AND friend_id = ?;`);
+		statement.run(myId, theirId);
+		db.prepare('COMMIT').run();
+		console.log(`Friend request success.`);
+		tryToApproveFriendship(myId, theirId);
+		return (true);
+	}
+	catch (error)
+	{
+		db.prepare('ROLLBACK').run();
+		console.error('Error: Cannot request friendship to create user.', error);
+		return (false);
+	}
+}
+
+
+
+export async function tryToApproveFriendship(myId: string, theirId: string): Promise<boolean>
+{
+	// Check they are ready for the next level with you. 
+	if (!checkAreTheyWaitingForFriendAproval(myId, theirId))
+		return (false);
+
+	try
+	{
+		db.prepare('BEGIN TRANSACTION').run();
+		let statement = db.prepare(`UPDATE friend SET friend_status = 1 WHERE my_id = ? AND friend_id = ?;`);
+		statement.run(myId, theirId);
+		statement.run(theirId, myId);
+		db.prepare('COMMIT').run();
+		console.log(`Friendship paperwork complete between ${myId} and ${theirId}`);
+		return (true);
+	}
+	catch (error)
+	{
+		db.prepare('ROLLBACK').run();
+		console.error(`Error: Cannot approve friendship, probally for the best.`, error);
 		return (false);
 	}
 }
